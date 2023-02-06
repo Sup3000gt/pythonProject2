ef198d5b5ced729586020b6106821f2774ce8aac
Sup3000gt <113158165+Sup3000gt@users.noreply.github.com>
2023-02-05 18:48:22 -0600

    Leetcode

---
+++ Kattis/Making A Meowth.py
@@ -0,0 +1,11 @@
+N, P, X, Y = map(int, input().split())  # map() function returns a map object(which is an iterator)
+                                        # of the results after applying the given function to each item
+                                        # of a given iterable (list, tuple etc.)
+                                        # map(fun, iter)
+
+# N = every Nth page Meowth read
+# P = total page you read
+# X = time for you to read a page
+# Y = time for Meowth read a page
+
+
--- Kattis/Soda Slurper.py
+++ Kattis/Soda Slurper.py
@@ -1,17 +1,13 @@
-Tims = input()
-Tims = Tims.split(" ")
-totalEmpty = int(Tims[0]) + int(Tims[1])
-recycle = int(Tims[2])
+Tims = input().split(" ")       # split input into a list
+totalEmpty = int(Tims[0]) + int(Tims[1])    # first two number equal or total bottle
+recycle = int(Tims[2])                      # third number is recycle requirements
 
+count = 0                                   # set a counter to count our
 
 # 9 0 3
-def drink(Empty,recycle):
-    newDrink = Empty // recycle
-    remainder = Empty % recycle
-    if newDrink + remainder < recycle:
-        return 0
-    else:
-        newDrink += drink(newDrink + remainder,recycle)
-    return newDrink
-
-print(drink(totalEmpty,recycle))
+while totalEmpty >= recycle:                # while our empty bottle is > recycle keep looping
+    newDrink = totalEmpty // recycle
+    count += newDrink                       # keep update our count
+    remainder = totalEmpty % recycle        # find out how many new drink we can recycle and reminder
+    totalEmpty = newDrink + remainder       # update new total empty bottle going back loop
+print(count)                                # return count
