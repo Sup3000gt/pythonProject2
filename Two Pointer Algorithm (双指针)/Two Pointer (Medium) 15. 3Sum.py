8c50ff0f8126bd83e529fed8fd58278de1e25f75
Sup3000gt <113158165+Sup3000gt@users.noreply.github.com>
2023-02-10 18:17:58 -0600

    Leetcode

--- Two Pointer Algorithm (双指针)/Two Pointer (Medium) 15. 3Sum.py
+++ Two Pointer Algorithm (双指针)/Two Pointer (Medium) 15. 3Sum.py
@@ -1,34 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) < 3: return []     # 如果数组小于3 那么直接返回空列表
-        ans = []                # 创建一个空的返回List用于储存结果
+        if len(nums) < 3: return []  # 如果数组小于3 那么直接返回空列表
+        ans = []  # 创建一个空的返回List用于储存结果
         n = len(nums)
-        nums.sort()             # 重新排列整个数组
+        nums.sort()  # 重新排列整个数组
 
         # 找出a + b + c = 0
         # a = nums[i], b = nums[left], c = nums[right]
 
-        for i in range(n-2):      # 开始遍历,因为 最少需要3个数所以i也就是我们的a只需要遍历到 列表的倒数第三个数即可
-            left = i+1
-            right = n-1
-            if nums[i] > 0:     # 因为列表排序过，所以 如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
+        for i in range(n - 2):  # 开始遍历,因为 最少需要3个数所以i也就是我们的a只需要遍历到 列表的倒数第三个数即可
+            left = i + 1
+            right = n - 1
+            if nums[i] > 0:  # 因为列表排序过，所以 如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
                 break
-            if i >= 1 and nums[i] == nums[i-1]:     # 去掉重复的a
+            if i >= 1 and nums[i] == nums[i - 1]:  # 去掉重复的a
                 continue
-            while left < right:     # 当左右指针还没相交的时候
+            while left < right:  # 当左右指针还没相交的时候
                 total = nums[i] + nums[left] + nums[right]
                 # total =  a      +    b    +     c
-                if total > 0:    # 在这里 因为a是恒定的 # 如果total > 0 说明,b+c 有点大了，那么就把右指针 左移
+                if total > 0:  # 在这里 因为a是恒定的 # 如果total > 0 说明,b+c 有点大了，那么就把右指针 左移
                     right -= 1
                 elif total < 0:  # 如果 < 0 ，说明 b+c 太小了， 那么就把左指针 右移
                     left += 1
-                else:           # 如果 不大也不小 那说明 a+b+c =0 那么就把 对应的 结果加入到ans结果集中
+                else:  # 如果 不大也不小 那说明 a+b+c =0 那么就把 对应的 结果加入到ans结果集中
                     ans.append([nums[i], nums[left], nums[right]])
-        # 接下来 要去掉重复的b和c
-                    while left != right and nums[left] == nums[left + 1]:   # 去掉重复的b
-                        left += 1
-                    while left != right and nums[right] == nums[right - 1]:  # 去掉重复的c
-                        right -= 1
-                    left += 1       # 最后把左指针右移
-                    right -= 1      # 右指针左移
-        return ans      # 最后返回 结果集
+                    # 接下来 要去掉重复的b和c
+                    while left != right and nums[left] == nums[left + 1]: left += 1  # 去掉重复的b
+                    while left != right and nums[right] == nums[right - 1]: right -= 1  # 去掉重复的c
+                    left += 1  # 最后把左指针右移
+                    right -= 1  # 右指针左移
+        return ans  # 最后返回 结果集
--- Two Pointer Algorithm (双指针)/Two Pointer (Medium) 18. 4Sum.py
+++ Two Pointer Algorithm (双指针)/Two Pointer (Medium) 18. 4Sum.py
@@ -1,2 +1,29 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # 解题思路跟 3Sum 一样 只不过多一层 for loop
+        nums.sort()       # 首先 把数组排序
+        n = len(nums)
+        res = []
+        for i in range(n-3):  # 然后还是遍历整个数组
+            # a + b + c + d = target
+            if i > 0 and nums[i] == nums[i - 1]:            # 对nums[i]去重,也就是去掉重复的a
+                continue
+            for k in range(i + 1, n-2):                     # 然后遍历k, 也就是b 因为b一定是在a后面 所以要从i+1开始遍历
+                if k > i + 1 and nums[k] == nums[k - 1]:
+                    continue                                # 对nums[k]去重, 去掉重复的b
+                p = k + 1               # 然后 思路就跟 3Sum 一样了左右指针， 左指针 = p , 右指针 = q
+                q = n - 1
+
+                while p < q:                            # 一直循环到左右指针相交
+                    if nums[i] + nums[k] + nums[p] + nums[q] > target:  # 跟 3Sum 一样 如果 大于 > target 右指针左移
+                        q -= 1
+                    elif nums[i] + nums[k] + nums[p] + nums[q] < target:    # 不然 左指针右移
+                        p += 1
+                    else:           # 如果都不是 那么就添加到最终返回的列表里
+                        res.append([nums[i], nums[k], nums[p], nums[q]])
+                        # 最后对nums[p]和nums[q]去重， 去掉重复的 c和d
+                        while p < q and nums[p] == nums[p + 1]: p += 1
+                        while p < q and nums[q] == nums[q - 1]: q -= 1
+                        p += 1      # 最后 左右指针 各移一位
+                        q -= 1
+        return res
