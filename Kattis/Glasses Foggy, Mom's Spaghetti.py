d14794031e61b6db00dfbcb22ed27118058a27c4
Sup3000gt <113158165+Sup3000gt@users.noreply.github.com>
2023-02-23 23:35:22 -0600

    Leetcode

--- Kattis/Glasses Foggy, Mom's Spaghetti.py
+++ Kattis/Glasses Foggy, Mom's Spaghetti.py
@@ -2,4 +2,12 @@ import math
 
 d, x, y, h = map(int, input().split())
 
-math.cos()
+bigTheta = math.atan((y+(h/2))/x)
+Theta1 = bigTheta - math.atan(y/x)
+Theta2 = bigTheta - math.atan((y-(h/2))/x)-Theta1
+LowDistance = math.tan(Theta1)*d
+HighDistance = math.tan(Theta2)*d
+
+ans = LowDistance + HighDistance
+
+print(ans)
