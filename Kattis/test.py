0c4f29dadf16dc6d624fe72f16570fd648eb24db
Sup3000gt <113158165+Sup3000gt@users.noreply.github.com>
2023-03-01 21:01:18 -0600

    Leetcode

--- Kattis/test.py
+++ Kattis/test.py
@@ -1,2 +1,15 @@
-N, K, M = map(int, input().split())
-print(N,M,K)
+n,p,m = map(int,input().split())
+counter = {input(): 0 for _ in range(n)}
+winners = []
+for _ in range(m):
+    name, score = input().split()
+    if counter[name] == -1:
+        continue
+    counter[name] += int(score)
+    if counter[name] >= p:
+        winners.append(name)
+        counter[name] = -1
+if not winners:
+    print('No winner!')
+else:
+    print('\n'.join((f'{name} wins!' for name in winners)))
